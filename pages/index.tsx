6955289acb09cc3ceae092068b6a1ba46a24bc62
Guillermo <memocargo@hotmail.com>
2023-08-15 23:01:51 -0600

    index cards created

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
+import type { NextPage } from "next";
+import { Layout } from "../components/layouts";
+
+const HomePage: NextPage = () => {
+	return (
+		<Layout title="Home - OpenJira">
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={4}>
+					<Card sx={{ height: "calc(100vh - 100px)" }}>
+						<CardHeader title="Pendientes" />
+						<CardContent>
+              
+            </CardContent>
+					</Card>
+				</Grid>
+				<Grid item xs={12} sm={4}>
+					<Card sx={{ height: "calc(100vh - 100px)" }}>
+						<CardHeader title="En progreso" />
+					</Card>
+				</Grid>
+				<Grid item xs={12} sm={4}>
+					<Card sx={{ height: "calc(100vh - 100px)" }}>
+						<CardHeader title="Completadas" />
+					</Card>
+				</Grid>
+			</Grid>
+		</Layout>
+	);
+};
+
+export default HomePage;
